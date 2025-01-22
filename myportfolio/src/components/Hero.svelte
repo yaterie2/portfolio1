6d9c64a588a0d033ff2934a45d58bef9a0de4112
yaterie2 <zubazo7ace@gmail.com>
2025-01-22 13:21:39 +0100

    Improvements mobile

--- myportfolio/src/components/Hero.svelte
+++ myportfolio/src/components/Hero.svelte
@@ -47,8 +47,8 @@
   <div class="about-content">
     <h2>About Me</h2>
     <p>
-I am a passionate digital product designer and developer, blending creativity with technical expertise to craft meaningful and engaging user experiences.<br>
-<br>Over four semesters at HfG Schwäbisch Gmünd, I honed my skills in uniting design and functionality, mastering the art of transforming complex ideas into seamless and impactful digital solutions.<br><br>My approach focuses on creating user-centered designs that not only look great but also perform flawlessly, ensuring every project delivers value and innovation.    </p>
+  I am a passionate digital product designer and developer, blending creativity with technical expertise to craft meaningful and engaging user experiences.<br>
+  <br>Over four semesters at HfG Schwäbisch Gmünd, I honed my skills in uniting design and functionality, mastering the art of transforming complex ideas into seamless and impactful digital solutions.<br><br>My approach focuses on creating user-centered designs that not only look great but also perform flawlessly, ensuring every project delivers value and innovation.    </p>
   </div>
 </section>
 
