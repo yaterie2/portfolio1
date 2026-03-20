60181ad902cd7b7ea733c318e2ce0a05986dcc34
yaterie2 <zubazo7ace@gmail.com>
2026-03-20 13:38:26 +0100

    copy deleted

--- myportfolio Kopie/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Navbar from '../components/Navbar/Navbar.svelte';
-  import Main from '../components/Main/Main.svelte';
-  import '../styles/global.scss';
-</script>
-
-<Navbar />
-<Main />
